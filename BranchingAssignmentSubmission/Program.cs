e2ec73db93df84dcde9298e483aba9226b718e93
sagisijandy <98680878+sagisijandy@users.noreply.github.com>
2023-04-13 22:22:13 -0700

    Add project files.

---
+++ BranchingAssignmentSubmission/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignmentSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+            Console.ReadLine();
+
+            
+            //Console.WriteLine("What is your Package weight?");
+            //string packageWeight = Console.ReadLine();
+            //int packweight = Convert.ToInt32(packageWeight);
+            //string result = packweight >= 50 ? "Package too heavy to be shipped via Package Express. Have a good day." : "Package Accepted";
+            //Console.WriteLine(result);
+            //Console.ReadLine;
+
+            //Console.WriteLine("What is your package width?");
+            //string packageWidth = Console.ReadLine();
+            //int packWidth = Convert.ToInt32(packageWidth);
+            //Console.WriteLine("your package width is:" + packageWidth);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your package height?");
+            //string packageHeight = Console.ReadLine();
+            //int packheight = Convert.ToInt32(packageHeight);
+            //Console.WriteLine("Your package height is: " + packageHeight);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your package Length?");
+            //string packageLength = Console.ReadLine();
+            //int packlength = Convert.ToInt32(packageLength);
+            //Console.WriteLine("Your package length is:" + packageLength);
+            //Console.ReadLine();
+
+            //int totalDImension = (packWidth * packheight * packlength);
+
+            //Console.WriteLine("What is the total dimension?");
+            //string totalDimension = Console.ReadLine();
+            //int totdimension = Convert.ToInt32(totalDimension);
+            //string resultTotal = totdimension >= 50 ? "Package too big to be shipped via Package Express. Have a good day." : "Package Accepted";
+            //Console.WriteLine(resultTotal);
+            //Console.ReadLine();
+
+            //var DollarValue = totalDimension % 100;
+
+            //Console.WriteLine(DollarValue);
+            //Console.ReadLine();
+
+
+            
+
+        }
+    }
+}
