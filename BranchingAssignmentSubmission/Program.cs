2de438afbcc53d27f92ef1ff7502c651a41ccb89
sagisijandy <98680878+sagisijandy@users.noreply.github.com>
2023-04-18 13:57:13 -0700

    commit

--- BranchingAssignmentSubmission/Program.cs
+++ BranchingAssignmentSubmission/Program.cs
@@ -16,47 +16,47 @@ namespace BranchingAssignmentSubmission
             Console.ReadLine();
 
             
-            //Console.WriteLine("What is your Package weight?");
-            //string packageWeight = Console.ReadLine();
-            //int packweight = Convert.ToInt32(packageWeight);
-            //string result = packweight >= 50 ? "Package too heavy to be shipped via Package Express. Have a good day." : "Package Accepted";
-            //Console.WriteLine(result);
-            //Console.ReadLine;
+            Console.WriteLine("What is your Package weight?");
+            string packageWeight = Console.ReadLine();
+            int packweight = Convert.ToInt32(packageWeight);
+            string result = packweight >= 50 ? "Package too heavy to be shipped via Package Express. Have a good day." : "Package Accepted";
+            Console.WriteLine(result);
+            Console.ReadLine();
 
-            //Console.WriteLine("What is your package width?");
-            //string packageWidth = Console.ReadLine();
-            //int packWidth = Convert.ToInt32(packageWidth);
-            //Console.WriteLine("your package width is:" + packageWidth);
-            //Console.ReadLine();
+            Console.WriteLine("What is your package width?");
+            string packageWidth = Console.ReadLine();
+            int packWidth = Convert.ToInt32(packageWidth);
+            Console.WriteLine("your package width is:" + packageWidth);
+            Console.ReadLine();
 
-            //Console.WriteLine("What is your package height?");
-            //string packageHeight = Console.ReadLine();
-            //int packheight = Convert.ToInt32(packageHeight);
-            //Console.WriteLine("Your package height is: " + packageHeight);
-            //Console.ReadLine();
+            Console.WriteLine("What is your package height?");
+            string packageHeight = Console.ReadLine();
+            int packheight = Convert.ToInt32(packageHeight);
+            Console.WriteLine("Your package height is: " + packageHeight);
+            Console.ReadLine();
 
-            //Console.WriteLine("What is your package Length?");
-            //string packageLength = Console.ReadLine();
-            //int packlength = Convert.ToInt32(packageLength);
-            //Console.WriteLine("Your package length is:" + packageLength);
-            //Console.ReadLine();
+            Console.WriteLine("What is your package Length?");
+            string packageLength = Console.ReadLine();
+            int packlength = Convert.ToInt32(packageLength);
+            Console.WriteLine("Your package length is:" + packageLength);
+            Console.ReadLine();
 
-            //int totalDImension = (packWidth * packheight * packlength);
+            int totalDImension = (packWidth * packheight * packlength);
 
-            //Console.WriteLine("What is the total dimension?");
-            //string totalDimension = Console.ReadLine();
-            //int totdimension = Convert.ToInt32(totalDimension);
-            //string resultTotal = totdimension >= 50 ? "Package too big to be shipped via Package Express. Have a good day." : "Package Accepted";
-            //Console.WriteLine(resultTotal);
-            //Console.ReadLine();
+            Console.WriteLine("What is the total dimension?");
+            string totalDimension = Console.ReadLine();
+            int totdimension = Convert.ToInt32(totalDimension);
+            string resultTotal = totdimension >= 50 ? "Package too big to be shipped via Package Express. Have a good day." : "Package Accepted";
+            Console.WriteLine(resultTotal);
+            Console.ReadLine();
 
-            //var DollarValue = totalDimension % 100;
+            var DollarValue = totalDimension;
 
-            //Console.WriteLine(DollarValue);
-            //Console.ReadLine();
+            Console.WriteLine(DollarValue);
+            Console.ReadLine();
 
 
-            
+
 
         }
     }
